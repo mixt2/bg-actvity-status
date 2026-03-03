e71c54adc374184a39a593ee893b3c894a05aa19
mixt2 <mistermixt@gmail.com>
2026-03-03 20:19:14 +0900

    alphateam.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Команды", "Майнкрафт"];
+const statusMessages = ["by movethatguns", "for Alpha Project"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
